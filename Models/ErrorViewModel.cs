d3fbda08ba7b5ea28be2a50f570ecd01675afe27
LOLA MATILDA NIETO <48588470@ort>
2024-07-04 08:14:48 -0300

    Create .NET MVC Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP5_SalaDeEscape_Nieto.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
