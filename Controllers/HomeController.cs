84d2bb5185e5762ed38f484c1d891876dea784ff
LOLA MATILDA NIETO <48588470@ort>
2024-08-07 14:34:13 -0300

    Models y Cotrollers listos

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,29 +2,64 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using TP5_SalaDeEscape_Nieto.Models;
 
+
 namespace TP5_SalaDeEscape_Nieto.Controllers;
+
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
     }
 
+
     public IActionResult Index()
     {
         return View();
     }
-public IActionResult CentralPerk()
+public IActionResult Comenzar()
+    {
+        int estadoJuego = Escape.GetEstadoJuego();
+        return View("Habitacion" + estadoJuego);
+
+    }
+    public IActionResult Tutorial()
     {
         return View();
     }
-    public IActionResult DeptoMonica()
+    public IActionResult Habitacion(int sala, string clave)
+    {
+      int estadoJuego = Escape.GetEstadoJuego();
+
+        //Para evitar que se pueda saltear salas manipulando informacion a traves de la URL
+    if(sala != estadoJuego){
+        estadoJuego = sala;
+        return View("Habitacion"+estadoJuego); 
+
+    }
+    if(Escape.ResolverSala(sala, clave)){ 
+        estadoJuego++;
+        if(estadoJuego == 6){
+                return RedirectToAction("Victoria");
+        } else{
+        return View("Habitacion" + estadoJuego);
+        }
+         
+       
+    } else{
+        return View();
+        ViewBag.Error = "La respuesta es incorrecta";
+    }
+    }
+    public IActionResult Victoria()
     {
         return View();
     }
+ 
     public IActionResult Privacy()
     {
         return View();
@@ -36,3 +71,6 @@ public IActionResult CentralPerk()
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
+
+
+
---
+++ Models/Escape.cs
@@ -0,0 +1,26 @@
+
+namespace TP5_SalaDeEscape_Nieto.Models;
+
+
+public static class Escape
+{
+    private static string[] incogitasSalas ;
+    private static int estadoJuego = 1;
+    private static void InicializarJuego(){
+        //Inicializa el juego con el array de incógnitas correctas ??
+        incognitasSalas =  new string [] {"3", "RJP", "331", "Smelly Cat - Ken Adams - Janice - Joey - Joey - Ursula", "Monica"};
+    }
+    public static int GetEstadoJuego(){
+        return estadoJuego;
+    }
+    public static bool ResolverSala(int Sala, string Incognita){
+        bool ret = false;
+        if(Sala == estadoJuego && Incognita == incogitasSalas[estadoJuego-1]){
+            ret = true;
+            estadoJuego++;
+        }
+        return ret;
+        //Tener en cuenta que si el array de incognitas tiene 0 elementos debe llamar a InicializarJuego (como ocurrio con el TP anterior --> que TP?)  ??
+    }
+}
+
